d00a4c549b79b0d3d32351bf00828ae3e3269770
Chriswpart <47011028+Chriswpart@users.noreply.github.com>
2020-03-04 09:45:54 -0800

    Updated querytree to hightlight active tree

---
+++ querytree.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.node').click(function() {
+        $(this).next('.hidden').slideToggle();
+        $(this).parent().siblings().find('ul').slideUp();
+        $(this).parent().find('.node').removeClass('active');
+        $(this).parent().siblings().find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
