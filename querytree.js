0941abde26cf8d20d2b32d4efe8aed68fd1c6c84
Chriswpart <47011028+Chriswpart@users.noreply.github.com>
2020-03-04 15:26:17 -0800

    Updated querytree to better fit plan

--- querytree.js
+++ querytree.js
@@ -6,7 +6,48 @@ $(document).ready(function () {
         $(this).parent().siblings().find('.active').removeClass('active');
         $(this).addClass('active');
     });
+
     $('.active').click(function () {
         console.log(this.id);
+    });
+
+    $("#committee5").change(function() {
+        if ($(this).data('options') === undefined) {
+          /*Taking an array of all options-2 and kind of embedding it on the select1*/
+          $(this).data('options', $('#subcommittee5 option').clone());
+        }
+        var id = $(this).val();
+        var options = $(this).data('options').filter('[value=' + id + ']');
+        $('#subcommittee5').html(options);
+    });
+
+    $("#committee6").change(function() {
+        if ($(this).data('options') === undefined) {
+          /*Taking an array of all options-2 and kind of embedding it on the select1*/
+          $(this).data('options', $('#subcommittee6 option').clone());
+        }
+        var id = $(this).val();
+        var options = $(this).data('options').filter('[value=' + id + ']');
+        $('#subcommittee6').html(options);
+    });
+
+    $("#committee7").change(function() {
+        if ($(this).data('options') === undefined) {
+          /*Taking an array of all options-2 and kind of embedding it on the select1*/
+          $(this).data('options', $('#subcommittee7 option').clone());
+        }
+        var id = $(this).val();
+        var options = $(this).data('options').filter('[value=' + id + ']');
+        $('#subcommittee7').html(options);
+    });
+
+    $("#committee8").change(function() {
+        if ($(this).data('options') === undefined) {
+          /*Taking an array of all options-2 and kind of embedding it on the select1*/
+          $(this).data('options', $('#subcommittee8 option').clone());
+        }
+        var id = $(this).val();
+        var options = $(this).data('options').filter('[value=' + id + ']');
+        $('#subcommittee8').html(options);
     });
 });
